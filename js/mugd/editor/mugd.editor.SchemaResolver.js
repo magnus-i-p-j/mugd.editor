eae97f9afc818eeda70902cb9718a541acd5669d
magnus-i-p-j <magnus.jonneryd@gmail.com>
2014-02-18 10:44:24 +0100

    First working (?) implementation.

--- js/mugd/editor/mugd.editor.SchemaResolver.js
+++ js/mugd/editor/mugd.editor.SchemaResolver.js
@@ -1,4 +1,5 @@
 goog.provide('mugd.editor.SchemaResolver');
+goog.require('goog.object');
 
 /**
  * @param {!Object} schema
@@ -14,27 +15,43 @@ mugd.editor.SchemaResolver = function (schema) {
  */
 mugd.editor.SchemaResolver.prototype.getSchema = function(schema){
   if(schema['links'] && schema['links']['rel'] === 'describedBy'){
-    var describedBy = schema['links']['rel']['href'];
-    describedBy = describedBy.slice(1);
-    schema = this._resolveSchema(this._schema, describedBy.split('/'));
+    var describedBy = schema['links']['href'];
+    describedBy = describedBy.slice(2);
+    var schemaTemplate = this._resolveSchema(this._schema, describedBy.split('/'));
+    this._inflateSchema(schemaTemplate, schema);
   }
   return schema;
 };
 
+/**
+ * @param {!Object} template
+ * @param {!Object} schema
+ * @private
+ */
+mugd.editor.SchemaResolver.prototype._inflateSchema = function(template, schema){
+  var title = schema['title'] || template['title'];
+  var description = schema['description'] || template['description'];
+  goog.object.forEach(template, function(value, key){
+    schema[key] = value;
+  });
+  schema['title'] = title;
+  schema['description'] = description;
+};
 
-  /**
-   * @param {!Object} context
-   * @param {Array.<string>} keys
-   * @returns {!Object}
-   * @private
-   */
-  mugd.editor.SchemaResolver.prototype._resolveSchema = function(context, keys){
-    var key = keys.shift();
-    context = context[key];
-    if(keys.length < 0){
-      return context;
-    }
-    else{
-      return this._resolveSchema(context, keys);
-    }
-  };
+
+/**
+ * @param {!Object} context
+ * @param {Array.<string>} keys
+ * @returns {!Object}
+ * @private
+ */
+mugd.editor.SchemaResolver.prototype._resolveSchema = function(context, keys){
+  var key = keys.shift();
+  context = context[key];
+  if(keys.length <= 0){
+    return context;
+  }
+  else{
+    return this._resolveSchema(context, keys);
+  }
+};
