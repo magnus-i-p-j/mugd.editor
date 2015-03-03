860bea26b6e18b7be205ccc0270c9bb106d07c08
Ulrik Långström <odalrick@gmail.com>
2015-03-03 10:05:28 +0100

    Create markdown binding and format

--- js/mugd/utils/mugd.utils.bindings.js
+++ js/mugd/utils/mugd.utils.bindings.js
@@ -60,3 +60,20 @@ mugd.utils.bindings.addUniqueId = function () {
   };
 
 };
+
+
+/**
+ * Copied from http://tech.pro/blog/1863/10-knockout-binding-handlers-i-don-t-want-to-live-without
+ * Creates a markdown binding
+ */
+mugd.utils.bindings.addMarkdown = function () {
+  ko.bindingHandlers['markdown'] = {
+    update: function(element, valueAccessor) {
+      return ko.bindingHandlers.html.update(element, function() {
+        return markdown.toHTML(ko.utils.unwrapObservable(valueAccessor()));
+      });
+    }
+  };
+};
+
+
